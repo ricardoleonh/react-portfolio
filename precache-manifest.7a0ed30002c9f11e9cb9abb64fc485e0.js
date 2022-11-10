8983aecb2398b710b21132cc53c3392f9abd60fa
ricardoleonh <ricardoleon_1@yahoo.com>
2022-11-10 11:10:44 -0600

    Updates

--- precache-manifest.7a0ed30002c9f11e9cb9abb64fc485e0.js
+++ precache-manifest.7a0ed30002c9f11e9cb9abb64fc485e0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6d64945cbb3e8c30ee2c",
+    "revision": "811867976a85d21a6cee",
     "url": "/react-portfolio/static/css/main.70f6dad6.chunk.css"
   },
   {
-    "revision": "6d64945cbb3e8c30ee2c",
-    "url": "/react-portfolio/static/js/main.6d64945c.chunk.js"
+    "revision": "811867976a85d21a6cee",
+    "url": "/react-portfolio/static/js/main.81186797.chunk.js"
   },
   {
     "revision": "405913c60ac2034e3149",
@@ -20,11 +20,11 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/media/cv.8db0e0c7.pdf"
   },
   {
-    "revision": "e4e98edde9dd2e9c95245c5565e2bfd8",
-    "url": "/react-portfolio/static/media/me.e4e98edd.png"
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/react-portfolio/static/media/me.0a8cf10e.png"
   },
   {
-    "revision": "d7bbe3e0f6fa415861f72dfd008bb099",
+    "revision": "1b0f784c0ffe8b0c5aaa6ba4ff53aac6",
     "url": "/react-portfolio/index.html"
   }
 ];
