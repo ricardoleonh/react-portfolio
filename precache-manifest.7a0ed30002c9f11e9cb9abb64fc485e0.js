f59df159274750c270cfc236c98a32804c7263f1
ricardoleonh <ricardoleon_1@yahoo.com>
2022-11-10 10:03:19 -0600

    Updates

---
+++ precache-manifest.7a0ed30002c9f11e9cb9abb64fc485e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "811867976a85d21a6cee",
+    "url": "/react-portfolio/static/css/main.70f6dad6.chunk.css"
+  },
+  {
+    "revision": "811867976a85d21a6cee",
+    "url": "/react-portfolio/static/js/main.81186797.chunk.js"
+  },
+  {
+    "revision": "405913c60ac2034e3149",
+    "url": "/react-portfolio/static/js/1.405913c6.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "8db0e0c71e419ff0892de3faed086470",
+    "url": "/react-portfolio/static/media/cv.8db0e0c7.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/react-portfolio/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "1b0f784c0ffe8b0c5aaa6ba4ff53aac6",
+    "url": "/react-portfolio/index.html"
+  }
+];
