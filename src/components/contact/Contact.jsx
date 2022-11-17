7fd95ac5dd246e1efd52f52819729697ec1f3332
Ricardo Leon <ricardoleon_1@yahoo.com>
2022-11-16 22:55:03 -0600

    contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMailOpen} from 'react-icons/hi'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <HiOutlineMailOpen />
+            <h4>Email</h4>
+            <h5>ricardoleon.h05@gmail.com</h5>
+            <a href="mailto:ricardoleon.gmail.com">Send an Email</a>
+          </article>
+        </div>
+        {/* End of Contact Options */}
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
